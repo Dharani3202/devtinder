864e1fece622d7a725a9d6e3d644a16d9ee2cb50
Dharani3202 <dharanibhargavibattini034@gmail.com>
2025-03-06 14:50:02 +0530

    Learnt get,post methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express=require("express");
 const app=express();
-app.use("/test",(req,res)=>{
-    res.send("yoo from the test")
+app.use("/user",(req,res)=>
+{
+    res.send("Youla");
 })
-app.use("/home",(req,res)=>{
-    res.send("yoo from the test")
+// now in the above if i  make a get api call in postman for localhost:3000/user it will send
+//  responsea as youla becuase app.use is first and then app.get is second in here so  order 
+// matters and also use will hanlde all type of http methods like get,post,put etc
+app.get("/user",(req,res)=>
+{
+    res.send({firstname:"Akshay",lastname:"saini"});
 })
+app.post("/user",(req,res)=>
+    {
+        res.send("Data successfully saved in db");
+    })
 app.listen(3000,()=>{console.log("Server is running")});
